ad9a3815fb67649767b831a69f280ee282b5180f
Ravi Yadav <shortanswer12345@gmail.com>
2022-08-26 13:18:14 +0530

    making the routes

--- index.js
+++ index.js
@@ -1,16 +1,67 @@
-const http = require('http')
+const http = require("http");
 
-const port = 3000
+const port = 3000;
+
+const toDoList = ["Complete Node ", "Play Cricket"];
 
 http
-.createServer((req,res) =>{
-    res.writeHead(200,{"Content-Type": "text/html"});
-    res.write("<h1>Hello ,this is my first server</h1>");
-    res.end()
+  .createServer((req, res) => {
+    const { method, url } = req;
+    // console.log(url);
+    // console.log(method);
+    if (url === "/todes") {
+      if (method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(toDoList.toString());
+      } else if (method === "POST") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.log(err);
+          })
+          .on("data", (chunk) => {
+            body += chunk;
+            console.log(chunk);
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
 
-})
-.listen(port,()=>{
+            let newTodo = toDoList;
+            newTodo.push(body.item);
+            console.log(newTodo);
+            res.writeHead(201);
+          });
+      } else if (method === "DELETE") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunk) => {
+            body += chunk;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            let deleteThis = body.item;
+            // for (let i =0;i<toDoList.length;i++){
+            //     if (toDoList[i] === deleteThis){
+            //         toDoList.slice(i,1);
+            //         break
+            //     }
+
+            // }
+            toDoList.find((element, index) => {
+              if (element === deleteThis) {
+                toDoList.splice(index, 1);
+              }
+            });
+          });
+      } else {
+        res.writeHead(501);
+      }
+    }
+    res.end();
+  })
+  .listen(port, () => {
     console.log(`Nodejs server started on port ${port}`);
-
-});
-
+  });
